533dd0bd48c42476765f9e8bff9b78b94eab0601
Lucasc4412 <31893773+Lucasc4412@users.noreply.github.com>
2017-09-19 22:09:59 -0400

    Add files via upload

---
+++ M2HW1_DistanceTraveled_LucasCatherine.py
@@ -0,0 +1,22 @@
+# CTI-110
+# M2HW1-Distance Traveled
+# Catherine Lucas
+# September 19,2017
+# This program displays total distance traveled.
+
+# Declare Variables
+
+SPEED = 70
+
+distancein6hours = SPEED * 6
+distancein10hours = SPEED * 10
+distancein15hours = SPEED * 15
+
+# Display total distance traveled 
+
+print('The total distance traveled in 6 hours is',distancein6hours,'miles per hour')
+print('The total distance traveled in 10 hours is',distancein10hours,'miles per hour')
+print('The total distance traveled in 15 hours is',distancein15hours,'miles per hour')
+
+
+
