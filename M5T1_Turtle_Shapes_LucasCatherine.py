133dc2cee8836101f88daf441895cbda11d32efb
Lucasc4412 <31893773+Lucasc4412@users.noreply.github.com>
2017-10-19 11:44:52 -0400

    Add files via upload

---
+++ M5T1_Turtle_Shapes_LucasCatherine.py
@@ -0,0 +1,64 @@
+# CTI-110
+# M5T1-Turtle shapes
+# Catherine Lucas
+# 10/19/17
+# This program makes shapes with a cute turle in a loop.
+
+
+def turtle():
+    import turtle
+    turtle.pencolor("pink")
+    turtle.bgcolor("red")
+    turtle.shape("turtle")
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(200)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.left(20)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.left(30)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.left(40)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+
+
+turtle()
+turtle()
+turtle()
+turtle()
+
+
+
